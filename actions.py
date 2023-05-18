148b0ececcd0c1ec953f773eb76bf02dcc9a6a5c
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-05-18 14:29:23 -0300

    feat: function to generate food

--- actions.py
+++ actions.py
@@ -1,3 +1,5 @@
+import random
+#
 def Food(worm):
     # TODO: Criar um gerador de localização da comida
     worm.append({'Coluna': 0, 'Linha': 0})
@@ -49,3 +51,24 @@ def Remove(worm):
     
     return worm
 #
+def GenerationFood(board, worm):
+    validationC = False
+    validationL = False
+    c = random.choice(range(0,len(board)))
+    l = random.choice(range(0,len(board)))
+    while not validationC and not validationL:      
+        for i in range(len(worm)):
+
+            if c == worm[i]['Coluna']:
+                print(f"coluna gerada: {c}, coluna minhoca:{worm[i]['Coluna']}")
+                c = random.choice(range(0,len(board)))
+            else:
+                validationC = True
+
+            if l == worm[i]['Linha']:
+                print(f"coluna gerada: {l}, coluna minhoca:{worm[i]['Linha']}")
+                l = random.choice(range(0,len(board)))
+            else:
+                validationL = True
+    food = {'Coluna': c, 'Linha': l}
+    return food
