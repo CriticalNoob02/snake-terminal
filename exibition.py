45709afe13f46b30e8141c84bbbcb68e9b2933fe
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-05-18 14:47:30 -0300

    feat: add food in board exibition

--- exibition.py
+++ exibition.py
@@ -5,8 +5,10 @@ def saveBoard(board, worm):
     for i in range(len(worm)):
         l = worm[i]['Linha']
         c = worm[i]['Coluna']
-        board[c][l] = ['O']
-
+        if i == 0:
+            board[c][l] = ['O']
+        else:
+            board[c][l] = ['o']
     return board
 #
 def clearBoard(board, worm):
@@ -16,9 +18,18 @@ def clearBoard(board, worm):
         board[c][l] = ['_']
 
     return board
+#
+def saveFood(board, food):
+    # TODO: chamar essa função antes da função saveBoard
+    l = food['Linha']
+    c = food['Coluna']
+    board[c][l] = ['f']
 
-def Console(board, worm):
+    return board
+#
+def Console(board, worm, food):
     os.system('clear')
+    board = saveFood(board, food)
     board = saveBoard(board, worm)
 
     for i in range(len(board)):
@@ -26,3 +37,4 @@ def Console(board, worm):
 
     board = clearBoard(board, worm)
     time.sleep(1)
+#
