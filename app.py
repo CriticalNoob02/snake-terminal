7f0ef6fdf6b5845cbe09225fefd1bed2bad267e2
CriticalNoob02 <adrieldchavea20@gmail.com>
2023-05-31 14:26:21 -0300

    feat: add new input moviment

--- app.py
+++ app.py
@@ -2,33 +2,56 @@ from controls.move import *
 from config.config import *
 from exibition.exibition import Console
 from actions.validations import *
+import threading 
 
-print('Digite o tamanho do board')
-value = int(input())
+tamanho = int(input('\n Digite o tamanho do board: '))
 
-# Configurando game;
-mesa = Board(value)
+# Configurando jogo;
+jogo = True
+tempo_limite = 2
+default = 'd'
+mesa = Board(tamanho)
 minhoca = Worm()
 comida = GenerationFood(mesa,minhoca)
 Console(mesa, minhoca, comida)
 
-algo = False
+#
+def InputMove():
+    global value 
+    value = input()  
+#
 
-while not algo:
+while jogo:
     try:
-        value = input().strip()
+        value = ''
+        leitor = threading.Thread(target=InputMove)
+
+        leitor.start()
+        leitor.join(tempo_limite)
+        
+        if leitor.is_alive():
+            value = default
+            pass
+        else: 
+            print(value)
+            pass
+
         match value:
             case 'a':
                 minhoca, comida = Left(minhoca, comida, mesa)
+                default = 'a'
             case 'w':
                 minhoca, comida = Up(minhoca, comida, mesa)
+                default = 'w'
             case 'd':
                 minhoca, comida = Right(minhoca, comida, mesa)
+                default = 'd'
             case 's':
                 minhoca, comida = Bottom(minhoca, comida, mesa)
+                default = 's'
             case _:
-                print('Errou')
+                print('Errou'+ value)
     except:
         print('Game Over - Perdeu playboy')
+        jogo = False
         break
-#
